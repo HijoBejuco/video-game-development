0c2f1ec49755e33625e9330e2cec8d32ea02648e
Alejandro Espinal Restrepo <alemon074@gmail.com>
2024-12-27 11:04:17 -0500

    doccumenting js and html files

--- game/js/game.js
+++ game/js/game.js
@@ -6,6 +6,10 @@ function selectCharacter() {
 
 // This creates the variable, using the getElementById
 // Here, we bring the element (button) from the html file
+// In this case, we bring the "select_character_button" button,
+// so we know exactly what's going on with it.
+// Here the class document, represents the whole html file, so
+// we can access to any element in the html file.
 let characterSelectionButton = document.getElementById("select_character_button")
 
 // Once we created the button variable, it has some methods,
