9ebe4ec918b8b48e0f3d9c58b1431a0507cdfc5a
Alejandro Espinal Restrepo <alemon074@gmail.com>
2025-01-08 15:54:08 -0500

    updating the function selectCharacter, so it alerts the user What character have been selected

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
 
 
 function gameInitializer() {
-    // This creates the variable, using the getElementById
-    // Here, we bring the element (button) from the html file
-    // In this case, we bring the "select_character_button" button,
-    // so we know exactly what's going on with it.
-    // Here the class document, represents the whole html file, so
-    // we can access to any element in the html file.
+    /* This creates the variable, using the getElementById
+    Here, we bring the element (button) from the html file
+    In this case, we bring the "select_character_button" button,
+    so we know exactly what's going on with it.
+    Here the class document, represents the whole html file, so
+    we can access to any element in the html file. */
     let characterSelectionButton = document.getElementById("select_character_button")
 
     // Once we created the button variable, it has some methods,
@@ -16,11 +16,31 @@ function gameInitializer() {
     characterSelectionButton.addEventListener("click", selectCharacter)
 }
 
+/* In this function we use the "document" class to bring elements by 
+their id, and then verify if that particular radio button is checked
+by the user. */
 function selectCharacter() {
-    alert("You choosed your character")
+
+    let inputHipo = document.getElementById("Hipo")
+    let inputCapi = document.getElementById("Capi")
+    let inputRat = document.getElementById("Rat")
+
+    if (inputHipo.checked) {
+        alert("You have selected Hipo")
+    }
+    else if (inputCapi.checked) {
+        alert("You have selected Capi")
+    }
+    else if (inputRat.checked) {
+        alert("You have selected Rat")
+    }
+    // In case no character is selected, we alert the user.
+    else {
+        alert("you must select a character")
+    }
 }
 
-// the window object represents the browser window, so here we 
-// are telling the browser to excecute the gameInitializer function
-// once the window is completely loaded.
+/* the window object represents the browser window, so here we 
+are telling the browser to excecute the gameInitializer function
+once the window is completely loaded.*/
 window.addEventListener("load", gameInitializer)
